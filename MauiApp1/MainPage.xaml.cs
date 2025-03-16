9d94a06b5a7f095f9e9651676bcc7fdaed4909e1
amrah <amrah@AMR>
2025-03-16 18:58:35 +0300

    Add project files.

---
+++ MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Maui.Controls;
+
+namespace MauiApp1
+{
+    public partial class MainPage : ContentPage
+    {
+        private string _currentInput = "";
+        private List<string> _expression = new();
+        private double _memory = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+       
+        private void OnDigitClicked(object sender, EventArgs e)
+        {
+            var button = sender as Button;
+            var digit = button.Text;
+
+            if (ResultLabel.Text == "0")
+                ResultLabel.Text = digit;
+            else
+                ResultLabel.Text += digit;
+
+            _currentInput += digit;
+        }
+
+       
+        private void OnOperatorClicked(object sender, EventArgs e)
+        {
+            var button = sender as Button;
+            var operation = button.Text;
+
+            if (!string.IsNullOrEmpty(_currentInput))
+            {
+                _expression.Add(_currentInput);
+                _expression.Add(operation);
+                _currentInput = "";
+            }
+
+            ResultLabel.Text += " " + operation + " ";
+        }
+
+       
+        private void OnEqualClicked(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(_currentInput))
+            {
+                _expression.Add(_currentInput);
+                _currentInput = "";
+            }
+
+            if (_expression.Count > 0)
+            {
+                double result = EvaluateExpression(_expression);
+                ResultLabel.Text = result.ToString();
+                _expression.Clear();
+            }
+        }
+
+       
+        private void OnClearClicked(object sender, EventArgs e)
+        {
+            _currentInput = "";
+            _expression.Clear();
+            ResultLabel.Text = "0";
+        }
+
+       
+        private void OnNegateClicked(object sender, EventArgs e)
+        {
+            if (double.TryParse(ResultLabel.Text, out double num))
+            {
+                ResultLabel.Text = (-num).ToString();
+                _currentInput = ResultLabel.Text;
+            }
+        }
+
+       
+        private void OnPercentClicked(object sender, EventArgs e)
+        {
+            if (double.TryParse(ResultLabel.Text, out double num))
+            {
+                ResultLabel.Text = (num / 100).ToString();
+                _currentInput = ResultLabel.Text;
+            }
+        }
+
+      
+        private void OnDecimalClicked(object sender, EventArgs e)
+        {
+            if (!_currentInput.Contains("."))
+            {
+                _currentInput += ".";
+                ResultLabel.Text += ".";
+            }
+        }
+
+      
+        private void OnMemoryClear(object sender, EventArgs e)
+        {
+            _memory = 0;
+        }
+
+        
+        private void OnMemoryRecall(object sender, EventArgs e)
+        {
+            ResultLabel.Text = _memory.ToString();
+            _currentInput = _memory.ToString();
+        }
+
+       
+        private void OnMemoryAdd(object sender, EventArgs e)
+        {
+            if (double.TryParse(ResultLabel.Text, out double num))
+            {
+                _memory += num;
+            }
+        }
+
+     
+        private void OnMemorySubtract(object sender, EventArgs e)
+        {
+            if (double.TryParse(ResultLabel.Text, out double num))
+            {
+                _memory -= num;
+            }
+        }
+
+      
+        private double EvaluateExpression(List<string> tokens)
+        {
+            List<string> newTokens = new();
+
+            
+            for (int i = 0; i < tokens.Count; i++)
+            {
+                if (tokens[i] == "×" || tokens[i] == "÷")
+                {
+                    double left = Convert.ToDouble(newTokens.Last());
+                    double right = Convert.ToDouble(tokens[++i]);
+                    newTokens.RemoveAt(newTokens.Count - 1);
+
+                    double intermediateResult = tokens[i - 1] == "×" ? left * right : left / right;
+                    newTokens.Add(intermediateResult.ToString());
+                }
+                else
+                {
+                    newTokens.Add(tokens[i]);
+                }
+            }
+
+          
+            double result = Convert.ToDouble(newTokens[0]);
+            for (int i = 1; i < newTokens.Count; i++)
+            {
+                if (newTokens[i] == "+")
+                {
+                    result += Convert.ToDouble(newTokens[++i]);
+                }
+                else if (newTokens[i] == "-")
+                {
+                    result -= Convert.ToDouble(newTokens[++i]);
+                }
+            }
+
+            return result;
+        }
+    }
+}
+    
---
+++ MauiApp1/NewPage1.xaml.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using Microsoft.Maui.Controls;
+
+namespace MauiApp1
+{
+    public partial class NewPage1 : ContentPage
+    {
+        private string currentInput = "";
+        private string operation = "";
+        private double firstNumber = 0;
+        private bool isNewEntry = true;
+
+        public NewPage1()
+        {
+            InitializeComponent();
+        }
+
+        private void OnNumberClicked(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            string number = button.Text;
+
+            if (isNewEntry)
+            {
+                currentInput = number;
+                isNewEntry = false;
+            }
+            else
+            {
+                currentInput += number;
+            }
+
+            DisplayLabel.Text = currentInput;
+        }
+
+        private void OnOperatorClicked(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            operation = button.Text;
+            firstNumber = double.Parse(currentInput);
+            isNewEntry = true;
+        }
+
+        private void OnEqualClicked(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(operation))
+            {
+                double secondNumber = double.Parse(currentInput);
+                double result = 0;
+
+                switch (operation)
+                {
+                    case "+":
+                        result = firstNumber + secondNumber;
+                        break;
+                    case "-":
+                        result = firstNumber - secondNumber;
+                        break;
+                    case "×":
+                        result = firstNumber * secondNumber;
+                        break;
+                    case "÷":
+                        result = secondNumber != 0 ? firstNumber / secondNumber : 0;
+                        break;
+                }
+
+                DisplayLabel.Text = result.ToString();
+                currentInput = result.ToString();
+                isNewEntry = true;
+            }
+        }
+
+        private void OnClearClicked(object sender, EventArgs e)
+        {
+            currentInput = "0";
+            firstNumber = 0;
+            operation = "";
+            isNewEntry = true;
+            DisplayLabel.Text = "0";
+        }
+
+        private void OnScientificFunctionClicked(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            string function = button.Text;
+            double number = double.Parse(currentInput);
+            double result = 0;
+
+            switch (function)
+            {
+                case "π":
+                    result = Math.PI;
+                    break;
+                case "e":
+                    result = Math.E;
+                    break;
+                case "x²":
+                    result = Math.Pow(number, 2);
+                    break;
+                case "√x":
+                    result = Math.Sqrt(number);
+                    break;
+                case "|x|":
+                    result = Math.Abs(number);
+                    break;
+                case "exp":
+                    result = Math.Exp(number);
+                    break;
+                case "mod":
+                    result = firstNumber % number;
+                    break;
+            }
+
+            DisplayLabel.Text = result.ToString();
+            currentInput = result.ToString();
+            isNewEntry = true;
+        }
+    }
+}
